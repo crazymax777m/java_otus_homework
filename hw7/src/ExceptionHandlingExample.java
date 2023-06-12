a9a282413223ba9d4ed911274945bff269ef6006
crazymax777m <maximus777m@mail.ru>
2023-06-12 23:06:13 +0300

    EDIT hw7

--- hw7/src/ExceptionHandlingExample.java
+++ hw7/src/ExceptionHandlingExample.java
@@ -3,18 +3,21 @@ import java.io.*;
 public class ExceptionHandlingExample {
     public static void main(String[] args) {
         try {
-            FileReader reader = new FileReader("C:\\Users\\Maxim\\Desktop\\otus_java_course\\hw7\\src\\input.txt");
-            FileWriter writer = new FileWriter("C:\\Users\\Maxim\\Desktop\\otus_java_course\\hw7\\src\\output.txt");
+            FileReader reader = new FileReader("hw7/src/input.txt");
+            FileWriter writer = new FileWriter("hw7/src/output.txt");
 
             processFile(reader, writer);
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Ошибка ввода-вывода: " + e.getMessage());
+        } catch (CustomException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static void processFile(FileReader reader, FileWriter writer) throws IOException {
+    public static void processFile(FileReader reader, FileWriter writer) throws IOException, CustomException {
         try (BufferedReader bufferedReader = new BufferedReader(reader);
              BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
 
@@ -28,7 +31,13 @@ public class ExceptionHandlingExample {
             throw e;
         } catch (Exception e) {
             System.out.println("Неизвестная ошибка: " + e.getMessage());
+            throw new CustomException("Произошла ошибка при обработке файла.", e);
         }
     }
 }
 
+class CustomException extends Exception {
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
