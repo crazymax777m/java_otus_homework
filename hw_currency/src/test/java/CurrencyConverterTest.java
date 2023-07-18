8db78f957aea0922534db600241b5211fefad34a
crazymax777m <maximus777m@mail.ru>
2023-07-18 18:36:20 +0300

    DONE hw for currency

--- hw_currency/src/test/java/CurrencyConverterTest.java
+++ hw_currency/src/test/java/CurrencyConverterTest.java
@@ -1,84 +1,26 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyConverterTest {
 
-    private CurrencyConverter currencyConverter;
-
-    @BeforeEach
-    void setUp() {
-        currencyConverter = new CurrencyConverter();
-    }
-
-    @Test
-    void convert1568() {
-        String expected = "одна тысяча пятьсот шестьдесят восемь рублей";
-        String actual = currencyConverter.convert(1568);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertMinus1568() {
-        String expected = "минус одна тысяча пятьсот шестьдесят восемь рублей";
-        String actual = currencyConverter.convert(-1568);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertZero() {
-        String expected = "ноль рублей";
-        String actual = currencyConverter.convert(0);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertMinusZero() {
-        String expected = "ноль рублей";
-        String actual = currencyConverter.convert(-0);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertMultipleZeroes() {
-        String expected = "ноль рублей";
-        String actual = currencyConverter.convert(0000000000000);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertMinusMultipleZeroes() {
-        String expected = "ноль рублей";
-        String actual = currencyConverter.convert(-0000000000000);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convert456546321() {
-        String expected = "четыреста пятьдесят шесть миллионов пятьсот сорок шесть тысяч триста двадцать один рубль";
-        String actual = currencyConverter.convert(456546321);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convertMinus956549684623() {
-        String expected = "минус девятьсот пятьдесят шесть миллиардов пятьсот сорок девять миллионов шестьсот восемьдесят четыре тысячи шестьсот двадцать три рубля";
-        String actual = currencyConverter.convert(-956549684623L);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convert0123() {
-        String expected = "сто двадцать три рубля";
-        String actual = currencyConverter.convert(0123D);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void convert41051() {
-        String expected = "сорок одна тысяча пятьдесят один рубль";
-        String actual = currencyConverter.convert(41051);
-        assertEquals(expected, actual);
+    @ParameterizedTest
+    @CsvSource({"1568, одна тысяча пятьсот шестьдесят восемь рублей",
+            "-1568, минус одна тысяча пятьсот шестьдесят восемь рублей",
+            "0, ноль рублей",
+            "-0, ноль рублей",
+            "0000000000000, ноль рублей",
+            "-0000000000000, ноль рублей",
+            "456546321, четыреста пятьдесят шесть миллионов пятьсот сорок шесть тысяч триста двадцать один рубль",
+            "-956549684623, минус девятьсот пятьдесят шесть миллиардов пятьсот сорок девять миллионов шестьсот восемьдесят четыре тысячи шестьсот двадцать три рубля",
+            "0123, сто двадцать три рубля",
+            "-0123, минус сто двадцать три рубля",
+            "41051, сорок одна тысяча пятьдесят один рубль"
+    })
+    public void testNum2strWithoutKopeks(String number, String expected) {
+        CurrencyConverter currencyConverter = new CurrencyConverter(number);
+        String result = currencyConverter.num2str(true);
+        assertEquals(expected, result);
     }
 }
